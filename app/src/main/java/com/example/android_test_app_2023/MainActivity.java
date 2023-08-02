4504f19cb8e9879ca3203a36ec8c268330e243c0
adnaan-said <adnaan009@gmail.com>
2023-08-02 17:01:49 +0400

    tests

--- app/src/main/java/com/example/android_test_app_2023/MainActivity.java
+++ app/src/main/java/com/example/android_test_app_2023/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.android_test_app_2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText numberArea;
+    Button search;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numberArea = findViewById(R.id.number_area_id);
+        search = findViewById(R.id.search_btn_id);
+
+
+
     }
 }
